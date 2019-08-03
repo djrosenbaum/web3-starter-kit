a8e7ccaec6da1a175b2222b06e64123dedb89a0d
DJ Rosenbaum <djrosenbaum@gmail.com>
2019-08-03 10:37:00 -0400

    adding core-js and common-js

--- client/rollup.config.js
+++ client/rollup.config.js
@@ -1,5 +1,6 @@
 const resolve = require('rollup-plugin-node-resolve');
 const babel = require('rollup-plugin-babel');
+const commonjs = require('rollup-plugin-commonjs');
 
 module.exports = {
   input: './src/js/main.js',
@@ -11,6 +12,7 @@ module.exports = {
 		resolve({
       browser: true,
     }),
+    commonjs(),
 		babel({
 			exclude: 'node_modules/**' // only transpile our source code
 		})
